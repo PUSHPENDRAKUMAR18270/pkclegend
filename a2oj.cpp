fbe4bfe80f4400d723cfdec890a184a5f4614307
PUSHPENDRAKUMAR18270 <53979418+PUSHPENDRAKUMAR18270@users.noreply.github.com>
2019-08-27 17:22:11 +0530

    Add files via upload
    This is my implementation of counting sort

---
+++ a2oj.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long int 
+/*using namespace std;
+int main()
+{
+   ll n,i,sum=0;
+   cin>>n;
+   ll arr[n];
+   for(i=0;i<n;i++)
+   {
+   	cin>>arr[i];
+   }
+   for(i=0;i<n;i++)
+   {
+      sum+=abs(arr[i]-(i+1));
+   }
+   cout<<sum;
+}*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	int s;
+
+   cout<<"we honour the work of the hardworking people"<<endl;
+   cin>>s;
+   cout<<s;
+}
